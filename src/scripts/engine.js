00fe34ab52b9c36683c12068deb0dde1a04c2368
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-30 19:01:00 -0300

    Finalizando o projeto com as funções do ckeckbox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,19 +1,9 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav");
-
-const playTune = (key) => {
-  audio.src = `src/tunes/${key}.wav`;
-  audio.play();
-
-  const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add("active");
-  setTimeout(() => {
-    clickedKey.classList.remove("active");
-  }, 150);
-};
 
 pianoKeys.forEach((key) => {
   mapedKeys.push(key.dataset.key);
@@ -26,8 +16,24 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+};
+
 const handleVolume = (e) => {
   audio.volume = e.target.value;
 };
 
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
 volumeSlider.addEventListener("input", handleVolume);
+keysCheck.addEventListener("click", showHideKeys);
